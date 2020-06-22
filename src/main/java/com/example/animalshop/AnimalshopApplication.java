dd7ae4705393c011c80ca87287d389a65260edf3
Dominik Baluch <baluch.dominik@gmail.com>
2020-06-22 14:32:55 +0000

    spring init

---
+++ src/main/java/com/example/animalshop/AnimalshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/animalshop/AnimalshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
